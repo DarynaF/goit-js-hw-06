1230aeb2b7694889342d80c45f009b8d082e8be0
DarynaF <karlu4ka333@gmail.com>
2022-12-02 01:20:29 +0300

    task1-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const recipeEl = [];
+
+const listEl = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const itemEL = document.createElement("li");
+  itemEL.classList.add("item");
+  itemEL.textContent = ingredient;
+  recipeEl.push(itemEL);
+});
+
+listEl.append(...recipeEl);
+
+console.log(recipeEl);
+
+
